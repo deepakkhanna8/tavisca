41d10464915b1679f9e6147b354b7a44b4df601c
deepakkhanna8 <59760013+deepakkhanna8@users.noreply.github.com>
2020-07-07 18:17:17 +0530

    Update card.component.ts

--- src/app/task-container/card/card.component.ts
+++ src/app/task-container/card/card.component.ts
@@ -36,6 +36,65 @@ export class CardComponent implements OnInit {
 public trackItem(index: number, item: ITaskList) {
     return item.id;
   }
+import { Component, Input, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { CreateCardComponent } from '../create-card/create-card.component';
+import { ICard } from '../models/card.model';
+import { ITaskList } from '../models/task.model';
+import { TaskListService } from '../services/task.list.service';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss']
+})
+export class CardComponent implements OnInit {
+
+  @Input() public task: ITaskList;
+  public cards: ICard[] = [];
+  public dialogRef: any;
+
+  constructor(public dialog: MatDialog,
+              private readonly service: TaskListService) { }
+
+  // tslint:disable-next-line: no-empty
+  public ngOnInit(): void {
+  }
+  public openCreateCard(model: ITaskList): void {
+    this.dialogRef = this.dialog.open(CreateCardComponent, {
+      data: {
+        task: model
+      }
+    });
+    this.dialogRef.afterClosed().subscribe((value: any) => {
+      console.log(`Dialog sent: ${value}`);
+      this.task = this.service.getTaskListById(model.id)[0];
+    });
+  }
+
+  public trackItem(index: number, item: ITaskList) {
+    return item.id;
+  }
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      if (event.container && event.previousContainer.data[0]) {
+        const card = JSON.parse(JSON.stringify(event.previousContainer.data[0]));
+        this.service.addCards(+event.container.id, card.name);
+        this.service.removeCards(+event.previousContainer.id, card.id);
+      }
+      transferArrayItem(event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex);
+    }
+  }
+
+}
+
 
   drop(event: CdkDragDrop<string[]>) {
     if (event.previousContainer === event.container) {
